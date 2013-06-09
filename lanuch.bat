899833fc121d8b9fa38298e2e52e94d5b69dec7d
mamboer <mamboer@live.com>
2013-06-09 18:02:27 +0800

    g

--- lanuch.bat
+++ lanuch.bat
@@ -3,4 +3,4 @@ set NWFOLDER=E:\Camp\Documents\GitHub\mamboer\nwapp\nw
 set NWEXE=%NWFOLDER%\nw.exe
 copy package.json %NWFOLDER%
 start "" %NWEXE% --url="file:///E:/Camp/Documents/GitHub/oxox/one/default.html"
-exit
+rem;;exit
