fa247192c35d308f61be363954b74e2855ac40e0
levin <vip_moon@126.com>
2013-06-15 02:29:01 +0800

    index.jade

--- app.js
+++ app.js
@@ -33,10 +33,25 @@ app.get('/users', user.list);
 
 var port = app.get('port'),
 	defaultHost = '127.0.0.1',
+	//C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe
+	nodemonDebugApp = 'E:\\Camp\\Documents\\GitHub\\mamboer\\nwapp\\nw\\nw.exe',
 	onListened = function(){
 		var address = server.address().address;
 		address = address==='0.0.0.0'?defaultHost:address;
-		window.location.replace('http://'+address+':'+port);
+		if (typeof(window) === 'undefined' ) {
+			//running in nodemon
+			//reference:http://nodejs.org/api/child_process.html#child_process_child_process_execfile_file_args_options_callback
+			var child = require('child_process').execFile(nodemonDebugApp,['--url=http://'+address+':'+port],function(err,stdout,stderr){
+				console.log('stdout:'+stdout);
+				console.log('stdin:'+stderr);
+				if (err!==null) {
+					console.log('error:'+err);
+				};
+			});
+		}else{
+			//running in node-webkit
+			window.location.replace('http://'+address+':'+port);
+		};
 	},
 	server = http.createServer(app).listen(port, onListened ),
 	tryCnt = 0;
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,12 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	res.render('index', { 
+		title: 'One!All-in-one Muramasa sword for front-end developer!',
+		appData:{
+			cnt:10
+		},
+		appName:'One!',
+		year:new Date().getFullYear()
+	});
 };
