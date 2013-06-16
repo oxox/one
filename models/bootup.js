4e8493a283eeaec0267be526fb55571b73783b0c
levin <vip_moon@126.com>
2013-06-17 00:58:30 +0800

    fix package.json

--- models/bootup.js
+++ models/bootup.js
@@ -2,6 +2,13 @@
  * site's base data api
  */
 module.exports = function(app){
-	var fs = require('fs-extra');
-	app.locals.package = fs.readJsonSync('package.json');
+	var fs = require('fs-extra'),
+		appData = {
+			package:fs.readJsonSync('package.json'),
+			userName:process.env['USERNAME'],
+			appRoot:process.execPath.substr(0,process.execPath.lastIndexOf('\\')+1)
+		};
+	appData.dataRoot = appData.appRoot+"data\\$\\".replace('$',appData.package.name);
+
+	app.locals(appData);
 };
