9da93e7c8d25f11a6f609a1810b5b101b42bc67c
mamboer <mamboer@live.com>
2013-07-03 01:27:20 +0800

    one clear up

--- lanuch.bat
+++ lanuch.bat
@@ -2,5 +2,5 @@
 set NWFOLDER=E:\Camp\Documents\GitHub\mamboer\nwapp\nw
 set NWEXE=%NWFOLDER%\nw.exe
 copy package-debug.json %NWFOLDER%\package.json
-start "" %NWEXE% --url="file:///E:/Camp/Documents/GitHub/oxox/one/default.html"
+start "" %NWEXE% --url="file:///E:/Camp/Documents/GitHub/oxox/one/assets/index.html"
 exit
