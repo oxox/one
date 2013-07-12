8ed2ae4abc9d6d76669c24ae38978322169d7881
mamboer <mamboer@live.com>
2013-07-12 12:27:20 +0800

    add app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+/**
+ * Offical applications for the One platform.
+ * @author levinhuang
+ */
+var OneAppList = [
+    {
+        "name":"ProjSpirit",
+        "desc":"A project management Node-Webkit application for front-end developers.",
+        "version":"1.0.0",
+        "url":"http://levinhuang-pc0.tencent.com:8081/one_test/projspirit.zip"
+    },{
+        "name":"HostSpirit",
+        "desc":"A host editor app of node-webkit",
+        "version":"1.0.1",
+        "url":"http://levinhuang-pc0.tencent.com:8081/one_test/hostspirit.zip"
+    },{
+        "name":"Yixun.TS",
+        "desc":"易迅前端页面片生成工具",
+        "version":"1.0.0",
+        "url":" http://ecd.oa.com/guide/ts/",
+        "target":"_blank"
+    }
+];
